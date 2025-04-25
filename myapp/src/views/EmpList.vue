f4569b62b4e6cdd409a375d78eef24a7517794ef
abcals77 <abcalsgh@gmail.com>
2025-04-25 17:48:32 +0900

    ts

---
+++ myapp/src/views/EmpList.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return { emps : []}
+  },
+  mounted(){
+    axios.get("/empdata.json")
+    .then(res => this.emps = res.data);
+  }
+}
+</script>
+<template>
+  <table>
+    
+  </table>
+</template>
